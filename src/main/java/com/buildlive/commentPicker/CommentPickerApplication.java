a372ad1a001e8b1981eab69436369e560cf3ffbf
Aravind <aravindmunna240@gmail.com>
2025-02-01 23:06:47 +0530

    first commit

---
+++ src/main/java/com/buildlive/commentPicker/CommentPickerApplication.java
@@ -0,0 +1,13 @@
+package com.buildlive.commentPicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentPickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildlive/commentPicker/CommentPickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildlive.commentPicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentPickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
