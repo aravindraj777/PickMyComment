348ca37ad9cf05f00c2b9bde1f650bdc59f2ff29
Aravind <aravindmunna240@gmail.com>
2025-02-02 23:22:44 +0530

    chor : Initial setup of package structure

---
+++ src/main/java/com/buildlive/commentPicker/youtube/controller/YoutubeCommentController.java
@@ -0,0 +1,9 @@
+package com.buildlive.commentPicker.youtube.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/youtube")
+public class YoutubeCommentController {
+}
---
+++ src/main/java/com/buildlive/commentPicker/youtube/service/YoutubeCommentService.java
@@ -0,0 +1,7 @@
+package com.buildlive.commentPicker.youtube.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class YoutubeCommentService {
+}
